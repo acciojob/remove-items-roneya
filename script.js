899da4aa0eb5b98c2755aeb454dbe6fb30d8bc49
Rohan Vidhate <49265832+roneya@users.noreply.github.com>
2023-04-13 23:08:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+ var colorSelect = document.getElementById("colorSelect");
+    var removeButton = document.getElementById("removeButton");
+    removeButton.addEventListener("click", function() {
+      var selectedOption = colorSelect.options[colorSelect.selectedIndex];
+      colorSelect.removeChild(selectedOption);
+    });
